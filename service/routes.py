ec9fcc45de4a93fd5e1302c1d07ab25caf6c8efb
cortedteate <carlos.cortesosorio@teatedigital.com>
2022-12-02 12:05:34 -0500

    completed-ci-linted-fix-2

--- service/routes.py
+++ service/routes.py
@@ -70,8 +70,7 @@ def list_accounts():
 
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-        
-    return jsonify(account_list),status.HTTP_200_OK
+    return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
@@ -88,7 +87,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
-    return account.serialize(),status.HTTP_200_OK
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
@@ -109,7 +108,7 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
 
-    return account.serialize(),status.HTTP_200_OK
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
@@ -126,7 +125,7 @@ def delete_accounts(account_id):
     if account:
         account.delete()
 
-    return "",status.HTTP_204_NO_CONTENT
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
